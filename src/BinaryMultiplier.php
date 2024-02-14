d31700a9e461345826f659c83992fd22eebf11df
a.kornienko <a.kornienko@xsolla.com>
2024-02-14 17:51:29 +0300

    add getMultipleProduct

---
+++ src/BinaryMultiplier.php
@@ -0,0 +1,12 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AKornienko\OtusComposerPackage;
+class BinaryMultiplier
+{
+    public function getMultipleProduct(int $number): int {
+        return 2 * $number;
+    }
+
+}
